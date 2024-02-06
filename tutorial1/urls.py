0f0bbd4b695ef447f433b26d5358df99dbb0bff9
Mohamed Dhleima <mohamed.dhleima@gmail.com>
2024-02-06 15:54:26 +0000

    set admin

--- tutorial1/urls.py
+++ tutorial1/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
 from snippets import views
-
+from django.contrib import admin
 # Create a router and register our ViewSets with it.
 router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet, basename='snippet')
@@ -10,5 +9,6 @@ router.register(r'users', views.UserViewSet, basename='user')
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path('', include(router.urls)),
 ]
